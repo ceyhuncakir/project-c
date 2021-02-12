c7480026a735f20c81c20c06e6746f2aa2b56f6d
Ceyhun Cakir <ceyhuncakir@live.nl>
2021-02-12 12:23:55 +0100

    - versie 0.3 mastermind
    - annotations gezet
    - eigen heuristiek verrandert op guess

--- 1. Mastering Mastermind/main.py
+++ 1. Mastering Mastermind/main.py
@@ -37,7 +37,7 @@ def rules():
     @====================== SPELREGELS ======================@\n
     1: Rode pin = Kleur komt voor in de code en staat op de juiste plek.
     2: Witte pin = Kleur komt voor in de code, maar staat op de onjuiste plek.
-    3: 1 = Wit, 2 = Magenta, 3 = Blauw, 4 = Geel, 5 = Groen, 6 = Rood.
+    3: 1 = Magenta, 2 = Wit, 3 = Blauw, 4 = Geel, 5 = Groen, 6 = Rood.
     4: Kraak de 4 kleurige code binnen 12 pogingen..\n
     @========================================================@\n
     ''', colors[randrange(5)]))
@@ -47,7 +47,7 @@ def type_of_algorithm():
     @====================== ALGORITME =======================@\n
     1: Knuth Algoritme.
     2: Simple Algoritme.
-    3: SOON.
+    3: Ozmen Algoritme (Eigen heuristiek).
     4: terug naar main menu\n
     @========================================================@\n
     ''', colors[randrange(5)]))
@@ -96,17 +96,20 @@ def algorithm_menu():
             code = input("\tGeef een code van 4 nummers op: ")
             simple_alg(code)
         elif option == 3:
-            print("SOON")
+            code = input("\tGeef een code van 4 nummers op: ")
+            ozmen_algoritme(code)
         elif option == 4:
             menu()
         else:
             print("\tKies uit 1, 2, 3 of 4.")
 
 def evalueren(guess, code):
-
+    # alles word opgetelt van code en guess en daar van kan je values pakken
     matches = sum((Counter(code) & Counter(guess)).values())
-    bulls = sum(c == g for c, g in zip(code, guess))
-    return bulls, matches - bulls
+    # alles opgetelt en gekeken of code gelijkwaardig is aan guess en daarvan worden het bepaald of het bulls zijn of niet
+    rode_pins = sum(c == g for c, g in zip(code, guess))
+    # rode pins worden gereturned met de matches - rode pins zodat er de witte pins uit komen
+    return rode_pins, matches - rode_pins
 
 def print_result(cnt, colorpins, feedback):
     print("\n\t#",cnt,"|", colored("X ", pins[colorpins[0]]) + "|", colored("X ", pins[colorpins[1]]) + "|", colored("X ", pins[colorpins[2]]) + "|", colored("X ", pins[colorpins[3]]) + "|", end=" ")
@@ -117,6 +120,8 @@ def colorpins_add(guess, colorpins):
     for item in range(0, len(guess), 1):
         colorpins.append(int(guess[item: item + 1]))
 
+def colorpins_clear(colorpins):
+    colorpins.clear()
 
 def player_vs_bot():
 
@@ -124,12 +129,15 @@ def player_vs_bot():
 
     # Kies 4 willekeurige kleuren.
     code = []
-    guess=[]
+    guess = []
 
+    #random generated codes die in het lijst worden gezet
     for i in range(0, 4):
         code.append(randrange(1, 7))
+
     print(colored("\tDe computer heeft een 4 kleurige code gekozen.\n", "yellow"))
 
+    #count
     cnt = 0
 
     # checked of de code niet gekraakt is door de codebreaker
@@ -140,10 +148,10 @@ def player_vs_bot():
             break
         else:
           cnt += 1
-
           guess = list(map(int, input("\tPoging {}: ".format(cnt))))
-          feedback = evaluate(guess, code)
 
+          #huidige guess word geevalueert
+          feedback = evalueren(guess, code)
           print_result(cnt, guess, feedback)
 
         # Alle kleuren zijn goed gegokt.
@@ -175,7 +183,7 @@ def knuth_alg(code):
 
         print_result(cnt, colorpins, feedback)
 
-        colorpins.clear()
+        colorpins_clear(colorpins)
 
         # hier word er gekeken of guess gelijkwaardig staat code
         if guess == code:
@@ -196,6 +204,7 @@ def knuth_alg(code):
 
 def simple_alg(code):
 
+    #internal list generated
     list_codes = [''.join(i) for i in product('123456', repeat=4)] # alle mogelijke codes
 
     guess = '1212'
@@ -213,7 +222,7 @@ def simple_alg(code):
 
         print_result(cnt, colorpins, feedback)
 
-        colorpins.clear()
+        colorpins_clear(colorpins)
 
         # hier word er gekeken of guess gelijkwaardig staat code
         if guess == code:
@@ -227,6 +236,48 @@ def simple_alg(code):
         else:
             #hier voert die het guess uit
             guess = list_codes[random.randint(0, random.randint(0, len(list_codes) - 1))]
+            cnt += 1
+
+def ozmen_algoritme(code):
+
+    list_codes = [''.join(i) for i in product('123456', repeat=4)] # alle mogelijke codes
+
+    randomgen = []
+
+    #random generated codes die in het lijst worden gezet
+    for i in range(0, 4):
+        randomgen.append(randrange(1, 7))
+
+    #random code word in het eerste guess gezet
+    guess = ''.join([str(elem) for elem in randomgen])
+
+    colorpins = []
+    cnt = 1
+
+    while True:
+        #huidige feedback van het guess
+        feedback = evalueren(guess, code)
+        # gaat over de lijst met alle mogelijke cobinaties heen en evalueert alle elementen in het lijst en kijkt of het gelijkwaardig staat aan het huidige guess feedback
+        list_codes = [code for code in list_codes if evalueren(guess, code) == feedback]
+
+        colorpins_add(guess, colorpins)
+
+        print_result(cnt, colorpins, feedback)
+
+        colorpins_clear(colorpins)
+
+        # hier wordt er gekeken of guess gelijkwaardig staat code
+        if guess == code:
+            print(colored("\tde code was opgelost in:", "green"), cnt, "pogingen! \n\t")
+            break
+
+        #hier word er gekeken of er nog een code in het list zit met mogelijke codes
+        if len(list_codes) == 1:
+            guess = list_codes[0]
+            cnt += 1
+        else:
+            #hier voert die het guess uit
+            guess = list_codes[-1]
             cnt += 1
 
 
@@ -249,13 +300,13 @@ def read_highscore():
         values = " - ".join(map(str, all))
         print("\t", values)
 
-
-def limit():
+def limit(code):
 
     try:
-        assert len(str(guess)) == 4
+        assert str(len(code)) == 4
     except AssertionError:
-        print("Voer precies 4 nummers in.")
+        print("\n\tVoer precies 4 nummers in.")
+        menu()
 
 if __name__ == '__main__':
     menu()
