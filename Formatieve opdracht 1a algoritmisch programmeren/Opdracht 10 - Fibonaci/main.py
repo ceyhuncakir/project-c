308fe04b052042426c4511ff1a7298d4c92c0709
Ceyhun Cakir <ceyhuncakir@live.nl>
2021-03-27 01:43:10 +0100

    Deleted All files except main.py

--- Formatieve opdracht 1a algoritmisch programmeren/Opdracht 10 - Fibonaci/main.py
+++
@@ -1,9 +0,0 @@
-waarde = int(input("geef een waarde: ")) # defineren we de input
-
-def recurssion(i):
-   if i <= 1: # word gekeken of i kleiner of gelijkwaardig staat aan 1
-       return i
-   else:
-       return(recurssion(i-1) + recurssion(i-2)) # hier word de recurssion uitgevoerd ookewel de waarde word in sequence gebracht
-for i in range(waarde): # hier doen we voor elke waarde in range van waarde
-     print(recurssion(i), end=", ")
